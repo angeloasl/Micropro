1b37fe1dfa99739897832e2f20741a3f423fbe9b
Angelo Lara <30839878+angeloasl@users.noreply.github.com>
2018-08-17 11:40:48 -0300

    Create lab32.ino

---
+++ lab32.ino
@@ -0,0 +1,43 @@
+byte ledAceso = 6; /* Inicia acendendo o led 6 */
+byte sentido = HIGH; /* valor de "sentido" define direção de rotação*/
+volatile int tempo=100;
+
+void setup() {
+  for(byte i=6; i <= 10; i++)
+  {
+     pinMode(i, OUTPUT);
+  } 
+  PCMSK2 = (1<<PCINT18)|(1<<PCINT19);
+  PCICR = (1<<PCIE2);
+ }
+
+void loop() {
+  digitalWrite(ledAceso, LOW); 
+  if (sentido == HIGH){
+     if(ledAceso == 6){   ledAceso = 10;  } 
+     else             {   ledAceso--;     }
+  } 
+  else{
+     if(ledAceso == 10){  ledAceso = 6;   }
+     else              {  ledAceso++;     }
+  }
+  digitalWrite(ledAceso,HIGH); 
+  delay(tempo); 
+}
+
+ISR(PCINT2_vect)
+{
+  if(digitalRead(3)==HIGH)
+    {
+  sentido=!sentido;
+    }
+  if(digitalRead(2)==HIGH)
+  if (tempo==100)
+  {
+  	tempo=500;
+  }
+  else
+  {
+    tempo=100;
+  }
+}
